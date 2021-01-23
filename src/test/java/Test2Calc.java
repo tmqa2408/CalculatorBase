cb86290fb670e5dbbd4f9ff245e420a5dc12da57
tmqa2408 <tmqa2408@gmail.com>
2021-01-23 15:09:32 -0700

    Init commit Test2Calc

---
+++ src/test/java/Test2Calc.java
@@ -0,0 +1,23 @@
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Test2Calc {
+    private Calculator underTest;
+
+    @BeforeTest
+    void setup() {
+        underTest = new Calculator();
+    }
+
+    //@DisplayName("Testing addition")
+    @Test
+    public void test1() {
+        final int first = 0;
+        final int second = 0;
+        final int expRes = 0;
+        final int actResult = underTest.sum(first, second);
+
+        Assert.assertEquals(actResult, expRes);
+    }
+}
